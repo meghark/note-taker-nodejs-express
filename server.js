15b6a18874412e7cd1083870623627dfc40c57f7
meghark <98137756+meghark@users.noreply.github.com>
2022-04-13 19:28:38 -0700

    UI connected to API for get and post

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const req = require('express/lib/request');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+const path = require('path');
 
 //Port setup for local and production environment
 const PORT = process.env.PORT || 3002;
@@ -11,13 +13,14 @@ const app = express();
 //Middleware setup. Parse all incoming string.
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static(path.join(__dirname+'/public')));
 
 //Route requests to api and html pages
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
+
 //Pull up static page
-app.use(express.static('public'));
 
 //set port to list to
 
